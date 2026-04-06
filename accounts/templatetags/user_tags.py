2cc30704a4f0038c38bca5a5b2f7b7ef4448bfb4
mpkosta <mpkosta@protonmail.com>
2026-04-06 21:42:40 +0300

    fix: Resolve 500 error by removing overriding admin logic in has_group filter

--- accounts/templatetags/user_tags.py
+++ accounts/templatetags/user_tags.py
@@ -6,6 +6,4 @@ register = template.Library()
 def has_group(user, group_name):
     if not user.is_authenticated:
         return False
-    if user.is_staff or user.is_superuser:
-        return True
     return user.groups.filter(name=group_name).exists()
